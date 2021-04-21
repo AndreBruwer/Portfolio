3b2f9449c9b4d0f1ed35e8a3380a43b2abfdd6dc
Andre_Bruwer <victorbruwer@gmail.com>
2021-04-21 07:47:51 +0200

    Base Game Works
    All that is left is the asteroids themselves and the interaction between the different elements

--- src/app/Components/asteroids/asteroids.component.ts
+++ src/app/Components/asteroids/asteroids.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import * as TL from '../../../assets/js/TweenLite.js';
-import * as EasePack from '../../../assets/js/EasePack.js';
+import { gsap, TweenMax } from 'gsap';
+// import * as TL from '../../../assets/js/TweenLite.js';
+// import * as EasePack from '../../../assets/js/EasePack.js';
 
 @Component({
   selector: 'app-asteroids',
@@ -17,99 +18,405 @@ export class AsteroidsComponent implements OnInit {
   ctx;
   game = false;
   rocks = [];
-  animateGame:boolean = true;
-
-  constructor() { }
+  animateGame:boolean = false;
+  shuttle: Shuttle;
+  lazarBolts = [];
+  gsap;
+  fps = 30;
+  frame;
+  stars = [];
+  constructor() {}
 
   ngOnInit(): void {
     this.canvas_container = document.getElementById("asteroid-canvas-container");
     this.canvas = document.getElementById("asteroid-canvas");
-    this.resize();
-  }
-  resize(){
     this.width = this.canvas_container.clientWidth;
     this.height = this.canvas_container.clientHeight;
-    this.canvas.style.width = this.width+"px";
-    this.canvas.style.height = (this.width*0.6)+"px";
+    this.canvas.width = this.width;
+    this.canvas.height = (this.width*0.6);
+    this.ctx = this.canvas.getContext('2d');
+
+    this.ctx.font = "30px Arial";
+    this.ctx.fillStyle = "white";
+    this.ctx.fillText("Click to Start game",this.canvas.width/3,this.canvas.height/2);
+  }
+  ngOnDestroy(){
+    document.onkeydown = null;
+    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
   }
 
 
 
+
+
+
+
+  resize(){
+    return;
+    this.width = this.canvas_container.clientWidth;
+    this.height = this.canvas_container.clientHeight;
+    this.canvas.width = this.width;
+    this.canvas.height = (this.width*0.6);
+  }
+  RotatePoint(cPoint, angle, point){
+    let s = Math.sin(angle*Math.PI/180);
+    let c = Math.cos(angle*Math.PI/180);
+    // translate point back to origin:
+    point.x -= cPoint.x;
+    point.y -= cPoint.y;
+    // rotate point
+    let xnew = point.x * c - point.y * s;
+    let ynew = point.x * s + point.y * c;
+    // translate point back:
+    point.x = xnew + cPoint.x;
+    point.y = ynew + cPoint.y;
+    return point;
+  }
+  GetDistance(point1, point2){
+    return Math.sqrt(Math.pow(point2.x-point1.x,2) + Math.pow(point2.y - point1.y,2));
+  }
 
   StartGame(){
-    if(this.game === false){
-      this.initGame();
-      this.initAnimation();
-    }
-  }
-  initGame(){
-    this.ctx = this.canvas.getContext('2d');
-    this.game = true;
-    
-    var px = 10;//position
-    var py = 10;
-    var xdir = 0;//direction
-    var ydir = 0;
-    var vx = (0) * xdir;//vector
-    var vy = (0) * ydir;
-    var p = { x: px, x_vector: vx, y: py, y_vector: vy, active: 1, circle: null};
-    this.rocks.push(p)
-
-    for (let i in this.rocks) {
-      this.rocks[i].circle = new Circle(this.rocks[i].px, this.rocks[i].py,2, 'rgba(255,255,255,1)', this.ctx);
-  }
-    
-  }
-  initAnimation() {
-    this.animate();
-    for (let i in this.rocks) {
-      this.shiftPoint(this.rocks[i]);
-    }
-  }
-  animate() {
-    if (this.animateGame) {
-        this.ctx.clearRect(0, 0, this.width, this.height);
-        // drawLines(points[0], points[1]);
-        for (var i in this.rocks) {
-            this.rocks[i].circle.draw();
+    if(this.animateGame == false){
+      this.animateGame = true;
+      this.shuttle = new Shuttle(this.canvas.width/2, this.canvas.height/2, this.ctx, this.canvas);
+      document.addEventListener("keydown", e => {
+        if(e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key == 'ArrowDown' || e.key == 'ArrowRight' || e.key == ' '){
+          e.preventDefault();
+          if (e.key === 'ArrowUp') {
+            //up arrow
+            this.accelerate();
+          }
+          if (e.key == 'ArrowDown') {
+            // down arrow
+            this.decelerate();
+          }
+          if (e.key === 'ArrowLeft') {
+            // left arrow
+            this.turnLeft();
+          }
+          if (e.key == 'ArrowRight') {
+            // right arrow
+            this.turnRight();
+          }
+          if (e.key == ' ') {
+            //space
+            this.shoot();
+          }
         }
+      });
+      for(let i = 0; i< 100; i++){
+        this.stars.push(new Star({x:Math.random()*this.canvas.width,y:Math.random()*this.canvas.height},this.ctx));
+      }
+      this.drawFrame(1000/this.fps, this.ctx);
+      this.canvas.scrollIntoView();
+      this.canvas.focus();
     }
-    // findNearest();
-    requestAnimationFrame(this.animate);
   }
-  shiftPoint(p) {
-    TL.TweenLite.to(p, 
-      2, 
-      { x: p.x + p.x_vector,
-      y: p.y + p.y_vector, 
-      ease: "Linear.easeNone",
-      onComplete: function () {
-      this.shiftPoint(p);
-      } }
-  );
+  StopGame(){
+    this.animateGame = false;
+    document.onkeydown = null;
+    clearTimeout(this.frame);
+    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+    this.ctx.font = "30px Arial";
+    this.ctx.fillStyle = "white";
+    this.ctx.fillText("Click to Start game",this.canvas.width/3,this.canvas.height/2);
   }
   
+//controls
+  turnLeft(){
+    this.shuttle.angle = this.shuttle.angle + 360/24*-1;
+    this.shuttle.turnShip();
+  }
+  turnRight(){
+    this.shuttle.angle = this.shuttle.angle + 360/24;
+    this.shuttle.turnShip();
+  }
+  accelerate(){
+    this.shuttle.Accelerate();
+  }
+  decelerate(){
+    this.shuttle.Decelerate();
+  }
+  shoot(){
+    if(this.lazarBolts.length <= 20)
+    this.lazarBolts.push(new LazarBolt(this.shuttle.core.x,this.shuttle.core.y,this.ctx,this.canvas, this.shuttle.angle));
+  }
 
+  drawFrame(ms:number,ctx){
+    ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+    if(this.animateGame == true){
+      //starts
+      this.stars.forEach(star =>{
+        star.draw();
+      })
+      //shuttle
+      this.shuttle.draw();
+      //projectiles
+      for(let i = 0; i < this.lazarBolts.length; i++){
+        let bolt = this.lazarBolts[i];
+        if(bolt.core.x < -20 || bolt.core.x > this.canvas.width+20 || bolt.core.y < -20 || bolt.core.y > this.canvas.height+20){
+          this.lazarBolts.splice(i,1);
+          i--;
+        }
+        else{
+          bolt.draw();
+        }
+      }
+      //boulders
+
+      //repeat
+      this.frame = setTimeout(() =>{this.drawFrame(ms,ctx)},ms)
+    }
+  }
 }
-class Circle {
-  posX;
-  posY;
-  rad;
-  color;
+class Rock{
+  core = {x:0,y:0};
+  points = [];
+  flamePoints = [];
+  angle = 0;
+  momentum = {x:0,y:0};
   ctx;
-  constructor(posX, posY, rad, color, ctx){
-    this.posX = posX;
-    this.posY = posY;
-    this.rad = rad;
-    this.color = color;
+  canvas;
+  radius;
+  constructor(){
+    
+  }
+}
+class Star{
+  core = {x:0,y:0};
+  ctx;
+  constructor(core, ctx){
+    this.core = core;
     this.ctx = ctx;
   }
-  draw() {
+  draw(){
+    let chance = Math.floor(Math.random()*100);
+    let outerSize = 7/2;
+    let innerSize = 4/2;
     this.ctx.beginPath();
-    this.ctx.arc(this.posX,this.posY, 0, 2 * Math.PI, false);
-    this.ctx.fillStyle = 'rgba(256,256,256,)';
-    this.ctx.fill();
-  };
+    this.ctx.moveTo(this.core.x-outerSize, this.core.y);
+    this.ctx.lineTo(this.core.x+outerSize, this.core.y);
+    this.ctx.strokeStyle = 'white';
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.core.x, this.core.y-outerSize);
+    this.ctx.lineTo(this.core.x, this.core.y+outerSize);
+    this.ctx.strokeStyle = 'white';
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.core.x-innerSize, this.core.y-innerSize);
+    this.ctx.lineTo(this.core.x+innerSize, this.core.y+innerSize);
+    this.ctx.strokeStyle = 'yellow';
+    if(chance <= 1)
+      this.ctx.strokeStyle = 'red';
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.core.x-innerSize, this.core.y+innerSize);
+    this.ctx.lineTo(this.core.x+innerSize, this.core.y-innerSize);
+    this.ctx.strokeStyle = 'yellow';
+    if(chance <= 1)
+      this.ctx.strokeStyle = 'red';
+    this.ctx.stroke();
+  }
 }
+class LazarBolt{
+  core = {x:0,y:0};
+  points = [];
+  angle = 0;
+  momentum = {x:0,y:0};
+  ctx;
+  canvas;
+  constructor(posX, posY, ctx, canvas, angle){
+    this.angle = angle;
+    this.canvas = canvas;
+    this.ctx = ctx;
+    //core
+    this.core = {x:posX,y:posY};
+
+    this.points.push({x:this.core.x,y:this.core.y-5});
+    this.points.push({x:this.core.x,y:this.core.y+5});
+    this.momentum = {x:0,y:-5};
+    this.momentum = this.RotatePoint({x:0,y:0},this.angle,this.momentum);
+  }
+  draw(){
+    length = 10;
+    this.core.x = this.core.x + this.momentum.x;
+    this.core.y = this.core.y + this.momentum.y;
+    this.points[0] = {x:this.core.x,y:this.core.y-(length/2)};
+    this.points[1] = {x:this.core.x,y:this.core.y+(length/2)};
+
+    for(let i = 0;i<this.points.length;i++){
+      this.points[i] = this.RotatePoint(this.core,this.angle,this.points[i]);
+    }
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.points[0].x, this.points[0].y);
+    this.ctx.lineTo(this.points[1].x, this.points[1].y);
+    this.ctx.strokeStyle = 'white';
+    this.ctx.stroke();
+  }
+  RotatePoint(cPoint, angle, point){
+    let s = Math.sin(angle*Math.PI/180);
+    let c = Math.cos(angle*Math.PI/180);
+    // translate point back to origin:
+    point.x -= cPoint.x;
+    point.y -= cPoint.y;
+    // rotate point
+    let xnew = point.x * c - point.y * s;
+    let ynew = point.x * s + point.y * c;
+    // translate point back:
+    point.x = xnew + cPoint.x;
+    point.y = ynew + cPoint.y;
+    
+    return point;
+  }
+}
+class Shuttle{
+  core = {x:0,y:0};
+  points = [];
+  flamePoints = [];
+  angle = 0;
+  momentum = {x:0,y:0};
+  ctx;
+  canvas;
+  flames:boolean = false;
+  constructor(posX, posY, ctx, canvas){
+    this.canvas = canvas;
+    this.ctx = ctx;
+    //core
+    this.core = {x:posX,y:posY};
+
+    this.points.push({x:this.core.x,y:this.core.y-20});
+    this.points.push({x:this.core.x+10,y:this.core.y+10});
+    this.points.push({x:this.core.x+3,y:this.core.y+10});
+    this.points.push({x:this.core.x+3,y:this.core.y+15});
+    this.points.push({x:this.core.x-3,y:this.core.y+15});
+    this.points.push({x:this.core.x-3,y:this.core.y+10});
+    this.points.push({x:this.core.x-10,y:this.core.y+10});
+    this.points.push({x:this.core.x,y:this.core.y-20});
+
+    this.flamePoints.push({x:this.core.x-5,y:this.core.y+18});
+    this.flamePoints.push({x:this.core.x+5,y:this.core.y+18});
+    this.flamePoints.push({x:this.core.x+7,y:this.core.y+25});
+    this.flamePoints.push({x:this.core.x+2,y:this.core.y+21});
+    this.flamePoints.push({x:this.core.x,y:this.core.y+25});
+    this.flamePoints.push({x:this.core.x-2,y:this.core.y+21});
+    this.flamePoints.push({x:this.core.x-7,y:this.core.y+25});
+  }
+  draw(){
+    this.core.x = this.core.x + this.momentum.x;
+    this.core.y = this.core.y + this.momentum.y;
+    this.points[0] = {x:this.core.x,y:this.core.y-20};
+    this.points[1] = {x:this.core.x+10,y:this.core.y+10};
+    this.points[2] = {x:this.core.x+3,y:this.core.y+10};
+    this.points[3] = {x:this.core.x+3,y:this.core.y+15};
+    this.points[4] = {x:this.core.x-3,y:this.core.y+15};
+    this.points[5] = {x:this.core.x-3,y:this.core.y+10};
+    this.points[6] = {x:this.core.x-10,y:this.core.y+10};
+    this.points[7] = {x:this.core.x,y:this.core.y-20};
+
+    this.flamePoints[0] = {x:this.core.x-5,y:this.core.y+18};
+    this.flamePoints[1] = {x:this.core.x+5,y:this.core.y+18};
+    this.flamePoints[2] = {x:this.core.x+7,y:this.core.y+25};
+    this.flamePoints[3] = {x:this.core.x+2,y:this.core.y+21};
+    this.flamePoints[4] = {x:this.core.x,y:this.core.y+25};
+    this.flamePoints[5] = {x:this.core.x-2,y:this.core.y+21};
+    this.flamePoints[6] = {x:this.core.x-7,y:this.core.y+25};
+    
+    this.turnShip();
+
+
+    this.ctx.beginPath();
+    this.points.forEach(point => {
+      this.ctx.lineTo(point.x,point.y);
+    })
+    this.ctx.fillStyle = 'white';
+    this.ctx.fill();
+
+    //flames
+    if(this.flames == true){
+      this.ctx.beginPath();
+      this.flamePoints.forEach(point => {
+        this.ctx.lineTo(point.x,point.y);
+      })
+      this.ctx.fillStyle = 'red';
+      this.ctx.fill();
+      this.flames = false;
+    }
+    //core
+    this.ctx.beginPath();
+    this.ctx.arc(this.core.x, this.core.y, 4, 0, 2 * Math.PI, false);
+    this.ctx.fillStyle = 'blue';
+    this.ctx.fill();
+    
+    //if outside of bounds
+    let buffer = 15;
+    if(this.core.x < -buffer){
+      this.core.x = this.canvas.width+buffer;
+    }
+    else if(this.core.x > this.canvas.width+buffer){
+      this.core.x = -buffer
+    }
+    if(this.core.y < -buffer){
+      this.core.y = this.canvas.height+buffer;
+    }
+    else if(this.core.y > this.canvas.height+buffer){
+      this.core.y = -buffer;
+    }
+  }
+  turnShip(){
+    for(let i = 0;i<this.points.length;i++){
+      this.points[i] = this.RotatePoint(this.core,this.angle,this.points[i]);
+    }
+    for(let i = 0;i<this.flamePoints.length;i++){
+      this.flamePoints[i] = this.RotatePoint(this.core,this.angle,this.flamePoints[i]);
+    }
+  }
+  Accelerate(){
+    let max = 20;
+    let accel = {x:0,y:-1};
+    accel = this.RotatePoint({x:0,y:0},this.angle,accel);
+    if(this.momentum.x + accel.x > -max && this.momentum.x + accel.x < max){
+      this.momentum.x = this.momentum.x + accel.x;
+    }
+    if(this.momentum.y + accel.y > -max && this.momentum.y + accel.y < max){
+      this.momentum.y = this.momentum.y + accel.y;
+    }
+    
+    this.flames = true;
+  }
+  Decelerate(){
+    let decel = 0.9;
+    this.momentum.x = this.momentum.x * decel;
+    this.momentum.y = this.momentum.y * decel;
+    if(this.momentum.x < 0.05 && this.momentum.x > -0.1){
+      this.momentum.x = 0;
+    }
+    if(this.momentum.y < 0.05 && this.momentum.y > -0.1){
+      this.momentum.y = 0;
+    }
+  }
+  RotatePoint(cPoint, angle, point){
+    let s = Math.sin(angle*Math.PI/180);
+    let c = Math.cos(angle*Math.PI/180);
+    // translate point back to origin:
+    point.x -= cPoint.x;
+    point.y -= cPoint.y;
+    // rotate point
+    let xnew = point.x * c - point.y * s;
+    let ynew = point.x * s + point.y * c;
+    // translate point back:
+    point.x = xnew + cPoint.x;
+    point.y = ynew + cPoint.y;
+    
+    return point;
+  }
+}
+
+
 
 
